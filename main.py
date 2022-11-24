c619646fce4d2924c998d761f0de08da274cfed3
TechnoCoder08 <102322220+TechnoCoder08@users.noreply.github.com>
2022-11-24 16:01:44 +0100

    Aktualizovat main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,56 @@
+def draw():
+    global display
+    display = Math.map(val, limit_min, limit_max, 1, 5)
+    basic.show_number(display)
+    if display == 1:
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        # # # # #
+        """)
+    elif display == 2:
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . . . .
+                        # # # # #
+                        # # # # #
+        """)
+    elif display == 3:
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        # # # # #
+                        # # # # #
+                        # # # # #
+        """)
+    elif display == 4:
+        basic.show_leds("""
+            . . . . .
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+        """)
+    elif display == 5:
+        basic.show_leds("""
+            # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+        """)
+    else:
+        pass
 val = 0
+display = 0
+limit_max = 0
+limit_min = 0
 OLED.init(128, 64)
+limit_min = 350
+limit_max = 400
 
 def on_forever():
     pass
@@ -7,7 +58,14 @@ basic.forever(on_forever)
 
 def on_every_interval():
     global val
-    val = pins.analog_read_pin(AnalogPin.P3)
+    val = pins.analog_read_pin(AnalogPin.P2)
+    draw()
+    if val > limit_max:
+        pins.digital_write_pin(DigitalPin.P12, 1)
+    elif val < limit_min:
+        pins.digital_write_pin(DigitalPin.P12, 0)
+    else:
+        pass
     OLED.write_num_new_line(val)
     OLED.new_line()
     OLED.new_line()
@@ -16,10 +74,4 @@ def on_every_interval():
     OLED.new_line()
     OLED.new_line()
     OLED.new_line()
-    if val > 400:
-        pins.digital_write_pin(DigitalPin.P2, 1)
-    elif val < 350:
-        pins.digital_write_pin(DigitalPin.P2, 0)
-    else:
-        pass
 loops.every_interval(200, on_every_interval)
--- main.ts
+++ main.ts
@@ -1,12 +1,14 @@
+function draw () {
+	
+}
+let display = 0
 let val = 0
 OLED.init(128, 64)
 let limit_min = 350
 let limit_max = 400
-basic.forever(function () {
-	
-})
 loops.everyInterval(200, function () {
-    val = pins.analogReadPin(AnalogPin.P4)
+    val = pins.analogReadPin(AnalogPin.P2)
+    display = Math.round(Math.map(val, limit_min, limit_max, 1, 5))
     OLED.writeNumNewLine(val)
     OLED.newLine()
     OLED.newLine()
@@ -15,10 +17,53 @@ loops.everyInterval(200, function () {
     OLED.newLine()
     OLED.newLine()
     OLED.newLine()
-    if (val > limit_max) {
-        pins.digitalWritePin(DigitalPin.P2, 1)
-    } else if (val < limit_min) {
-        pins.digitalWritePin(DigitalPin.P2, 0)
+    if (display == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+    } else if (display == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            `)
+    } else if (display == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (display == 4) {
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (display == 5) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+    	
+    }
+    if (val >= limit_max) {
+        pins.digitalWritePin(DigitalPin.P12, 1)
+    } else if (val <= limit_min) {
+        pins.digitalWritePin(DigitalPin.P12, 0)
     } else {
     	
     }
